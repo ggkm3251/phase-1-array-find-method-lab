c6a7594e1a9d252d31be94036275c1fa29b52f7a
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-10 15:57:03 +0300

    commit tree cac965ee1627cde99fe9e0785d3c5194422077e4

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+/*function superbowlWin(record) {
+    //return (element % 2 === 1);
+    return (record === '2015')
+}
+record.find(superbowlWin);
+
+function superbowlWin(record) {
+    //return (element % 2 === 1);
+    return (record === 'W')
+  }
+
+
+superbowlWin = (record) => {
+    const outcome = record.find( record => record.outcome === "W" );
+}
+
+superbowlWin = (record) => {
+    return !!outcome ? outcome.year : undefined;
+}
+
+superbowlWin = (record) => {
+    const outcome = record.find( record => record.outcome === "W" );
+}
+superbowlWin = (sadReality) => {
+    const outcome = sadReality.find( sadReality => sadReality.outcome === "N/A" );
+}*/
+
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+}
